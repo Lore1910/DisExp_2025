1b81c54a52ba6415bd850523c226609b7e78cd24
Laura Lorena Camacho Rangel <laura.camachor@uanl.edu.mx>
2025-05-07 10:38:08 -0600

    practica 6

---
+++ PRACTICA 6.R
@@ -0,0 +1,41 @@
+# Laura Lorena Camacho Rangel
+# 2070458
+# 07/05/2025
+
+# Importar datos de internet
+ur1 <- "https://raw.githubusercontent.com/mgtagle/Exp_Met_Est_AD2023/refs/heads/main/Scripts/localidades.csv"
+
+
+Datos <- read.csv(ur1, header = T)
+Datos$Paraje <- as.factor(Datos$Paraje)
+
+boxplot(Datos$DAP ~ Datos$Paraje,
+        col = "pink",
+        xlab = "Localidades",
+        ylab = "DAP (CM)")
+
+tapply(Datos$DAP, Datos$Paraje, mean)
+tapply(Datos$DAP, Datos$Paraje, var)
+
+shapiro.test(Datos$DAP)
+bartlett.test(Datos$DAP ~ Datos$Paraje)
+
+Datos$tlog <- log10(Datos$DAP+1)
+
+shapiro.test(Datos$tlog)
+
+Datos$tsqrt <- sqrt(Datos$DAP)
+shapiro.test(Datos$tsqrt)
+bartlett.test(Datos$tsqrt ~ Datos$Paraje)
+
+boxplot(Datos$tsqrt ~ Datos$Paraje,
+        col = "pink",
+        xlab = "Localidad",
+        ylab = "DAP (cm)")
+
+# Iniciar con el AOV
+par.aov <- aov(Datos$tsqrt ~ Datos$Paraje)
+summary(par.aov)
+
+TukeyHSD(par.aov)
+plot(TukeyHSD(par.aov))
