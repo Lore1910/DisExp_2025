f02bba7b7fff70fa78ce87f0016af270848c1581
Laura Lorena Camacho Rangel <laura.camachor@uanl.edu.mx>
2025-05-09 09:00:05 -0600

    update

--- Laboratorio 4.R
+++ Laboratorio 4.R
@@ -13,36 +13,22 @@ costal <- c(87.7, 80.01, 77.28, 78.76, 81.52, 74.4, 80.71, 79.5, 77.87, 81.94, 8
              78.89, 77.14, 69.94, 78.54, 79.7, 82.45, 77.29, 75.52, 77.21, 75.99,
              81.94, 80.41, 77.77)
 mean(costal)
-## [1] 78.91682
+
 
 # Length = amplitud de los datos
 length(costal)
-## [1] 44
+
 
 shapiro.test(costal)
-##
-## Shapiro-Wilk normality test
-##
-## data:  costal
-## W = 0.97851, p-value = 0.5752
+
 
 boxplot(costal)
 
 # Fivenum = mostrar valores de los cuartiles
 fivenum(costal)
-## [1] 69.940 77.245 78.950 80.705 87.700
+
 
 # El siguiente codigo es para La prueba de t de una sola muestra 
 # mu = La media de referencia
 t.test(costal, mu= 80)
-##
-## One Sample t-test
-##
-## data:  costal
-## t = -2.357, df = 43, p-value = 0.02305
-## alternative hypothesis: true mean is not equal to 80
-## 95 percent confidence interval:
-##  77.99003 79.84361
-## sample estimates:
-##  mean of x 
-## 78.91682 
+
