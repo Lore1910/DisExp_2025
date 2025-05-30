f390545bea28718654b5b89fa131b58d8f6e95f6
Lore1910 <laura.camachor@uanl.edu.mx>
2025-05-30 12:03:43 -0600

    Add files via upload

---
+++ Examen_Final-DE.R
@@ -0,0 +1,26 @@
+# Laura Lorena Camacho Rangel
+# 2070458
+# 30/05/2025
+
+## Examen final
+
+# Importar datos de internet
+ur1 <- "https://stat.ethx.ch/R-devel/library/datasets/html/InsectSprays.html"
+
+data("InsectSprays")
+head(InsectSprays)
+
+tapply(InsectSprays$count, InsectSprays$spray, length)
+
+boxplot(InsectSprays$count ~ InsectSprays$spray,
+        main = "Conteo de insectos por tipo spray",
+        col = "pink",
+        xlab = "Spray",
+        ylab = "Contenido de insectos")
+
+tapply(InsectSprays$count, InsectSprays$spray, mean )
+tapply(InsectSprays$count, InsectSprays$spray, var)
+
+shapiro.test(InsectSprays$count)
+
+bartlett.test(count ~ spray, data = InsectSprays)
